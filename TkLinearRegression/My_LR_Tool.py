1cceccf159ffab7b9a1e252d193956d2b93fe171
onezmy <cyonezmy@gmail.com>
2023-04-24 17:17:35 -0700

    Final draft

--- TkLinearRegression/My_LR_Tool.py
+++ TkLinearRegression/My_LR_Tool.py
@@ -9,12 +9,16 @@ Created on Sun Apr  9 17:11:02 2023
 import os
 import numpy as np
 import pandas as pd
+import time
+
 
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 from tkinter.tix import *
 import tkinter.font as tkFont
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 from sklearn.linear_model import LinearRegression
 
@@ -57,11 +61,12 @@ def add():
     """
     function for add button, update the data then show new data in window
     """
+    global data
     x = input_X.get()
     y = input_Y.get()
     X.append(x)
     Y.append(y)
-    data.append(tuple([x,y]))
+    data.insert(0,tuple([x,y]))
     show_all()
     points_plot()
     
@@ -91,6 +96,8 @@ def points_plot():
     """
     function to pliot the scatter plot of current data
     """
+    clear_points_plot()
+    
     X = list(list_X.get(0,list_X.size()-1))
     Y = list(list_Y.get(0,list_Y.size()-1))
     if len(X) == 0:
@@ -116,7 +123,7 @@ def points_plot():
 
     canvas1 = FigureCanvasTkAgg(fig,master=window)
     canvas1.draw()
-    canvas1.get_tk_widget().pack(padx=10, pady=20)
+    canvas1.get_tk_widget().pack(padx=800, pady=10)
 
     
     
@@ -158,9 +165,12 @@ def regression_plot():
     """
     function for start button, plot the regression gif
     """
-    fig = plt.figure(figsize = (9, 9),dpi=50)
+    clear_points_plot()
+
+    fig = plt.figure(figsize = (13, 13),dpi=50)
     ax=fig.add_subplot()
-    fig2 = plt.figure(figsize=(9, 9),dpi=50)
+    fig2 = plt.figure(figsize=(13, 13),dpi=50)
+    ax2 = fig2.add_subplot()
     X_num, Y_num = [], []
     n = len(X)
     for i in range(n):
@@ -184,6 +194,8 @@ def regression_plot():
     ims_loss = []
     X_num = np.array(X_num)
     Y_num = np.array(Y_num)
+    iteration_lsit = []
+    loss_list = []
     for i in range(epochs): 
         Y_pred = m * X_num + c  # The current predicted value of Y
         D_m = (-2/n) * sum(X_num * (Y_num - Y_pred))  # Derivative wrt m
@@ -192,25 +204,39 @@ def regression_plot():
         c = c - LR * D_c  # Update c
         m_list.append(m)
         c_list.append(c)
+        plt.rcParams["axes.titlesize"]='xx-large'
         ax.scatter(X_num,Y_num, c = '#7E95F2', marker = '.')
         im, = ax.plot(x,lr(m,c),'r')
         function_exp = "y = "+str(m)+" * x + "+str(c)
         title= ax.text(0.5,1.05,"iteration = " + str(i) + "\n "+function_exp, 
                         size=plt.rcParams["axes.titlesize"],
-                        ha="center", transform=ax.transAxes, )
+                        ha="center", transform=ax.transAxes,)
         ims.append([im,title])
-        
+        iteration_lsit.append(i)
+        cur_loss = 1/n * sum(Y_num - Y_pred)
+        loss_list.append(cur_loss)
+        ax2.axis(xmin=0,xmax=epochs,ymin=0,ymax=1.2*loss_list[0])
+        im2, = ax2.plot(iteration_lsit,loss_list,c='r')
+        title2 = ax2.text(0.5,1.05,"loss over iteration \n current loss ="+str(cur_loss), 
+                        size=plt.rcParams["axes.titlesize"],
+                        ha="center", transform=ax.transAxes, )
+        ims_loss.append([im2,title2])
         if i%10 == 0:
             print('Processing iteration', i)
+            
     print('Saving to gif')
     ani = animation.ArtistAnimation(fig, ims, interval=50, blit=False)
     ani.save("test.gif",writer='pillow')
+    
+    ani2 = animation.ArtistAnimation(fig2, ims_loss, interval=50, blit=False)
+    ani2.save("test_loss.gif",writer='pillow')
     print('Done')
-    lbl = ImageLabel(window)
-    lbl.pack()
-    lbl.place(x=475,y=20,width=500)
+    
+    global lbl, lbl2
+
     lbl.load('test.gif')
-        
+    lbl2.load('test_loss.gif')
+    
         
         
 def load_data():
@@ -241,20 +267,31 @@ def save_data():
         output_file_name += '.csv'
     pd.DataFrame(data).to_csv(output_file_name, index=False)
 
+def clear_data():
+    X = []
+    Y = []
+    data = []
+    df = {}
+    clear_points_plot()
+    list_X.delete(0,END)
+    list_Y.delete(0,END)
+
 """
 init window
 """
-window = Tk()
+
+window = ttk.Window()
 window.title("Linear Regression")
-window.geometry("1400x500")
+window.geometry("2000x800")
 
 
 
 """
 define fonts
 """
-font_yh1 = tkFont.Font(family='microsoft yahei', size=12, weight='bold')
-font_yh2 = tkFont.Font(family='microsoft yahei', size=8, weight='bold')
+font1 = tkFont.Font(size=10)
+font2 = tkFont.Font(size=12, weight='bold')
+
 
 
 
@@ -263,93 +300,78 @@ add buttons/labels
 """
 
 # 'Enter X' label
-Enter_X = tk.Label(window,text="Enter X: ",anchor="w", font=font_yh1)
-Enter_X.place(x=50,y=50,width=100)
-input_X = Entry(window)
-input_X.place(x=130,y=55,width=100)
+Enter_X = ttk.Label(window,text="Enter X :",anchor="w", background = '#fff', font = font1)
+Enter_X.place(x=50,y=50,width=120)
+input_X = ttk.Entry(window, cursor=	'left_ptr')
+input_X.place(x=180,y=50,width=150)
 
 # 'Enter Y' label
-Enter_Y = tk.Label(window,text="Enter Y: ",anchor="w", font=font_yh1)
-Enter_Y.place(x=50,y=75,width=100)
-input_Y = Entry(window)
-input_Y.place(x=130,y=80,width=100)
+Enter_Y = ttk.Label(window,text="Enter Y :",anchor="w", background = '#fff', font = font1)
+Enter_Y.place(x=50,y=100,width=120)
+input_Y = ttk.Entry(window, cursor=	'left_ptr')
+input_Y.place(x=180,y=100,width=150)
 
 # 'Learning Rate' label
-Learning_Rate = tk.Label(window,text="Learning Rate: ",anchor="w", font=font_yh1)
-Learning_Rate.place(x=250,y=50,width=100)
-input_LR = Entry(window)
-input_LR.place(x=350,y=55,width=100)
+Learning_Rate = ttk.Label(window,text="Learning Rate:",anchor="w", background = '#fff', font = font1)
+Learning_Rate.place(x=50,y=210,width=180)
+input_LR = ttk.Entry(window)
+input_LR.place(x=240,y=210,width=90)
 input_LR.insert(0,'0.01')
 
 # 'Max Iter' label
-Max_iter = tk.Label(window,text="Max Iter: ",anchor="w", font=font_yh1)
-Max_iter.place(x=250,y=75,width=100)
-input_Max_iter = Entry(window)
-input_Max_iter.place(x=350,y=80,width=100)
+Max_iter = ttk.Label(window,text="Max Iteration:",anchor="w", background = '#fff', font = font1)
+Max_iter.place(x=50,y=260,width=180)
+input_Max_iter = ttk.Entry(window)
+input_Max_iter.place(x=230,y=260,width=100)
 input_Max_iter.insert(0,'50')
 
 # 'Add' button
-Add = Button(window,text="Add",command=add)
-Add.place(x=50,y=125,width=87)
+Add = ttk.Button(window,text="Add",command=add,bootstyle = ttk.constants.PRIMARY)
+Add.place(x=50,y=155,width=130)
 
 # 'Remove' button 
-Remove = Button(window,text="Remove",command=remove)
-Remove.place(x=150,y=125,width=87)
-
-# 'Start' button 
-Start = Button(window,text="Start",command=regression_plot)
-Start.place(x=250,y=125,width=87)
-
-# 'Stop' button ######################################################
-Stop = Button(window,text="Stop",command=add)
-Stop.place(x=350,y=125,width=87)
+Remove = ttk.Button(window,text="Remove",command=remove,bootstyle = ttk.constants.DANGER)
+Remove.place(x=200,y=155,width=130)
 
 # 'Load Data' button 
-Load_data = Button(window,text="Load Data :", font=font_yh2, command=load_data)
-Load_data.place(x=200,y=195,width=87)
-input_data_file = Entry(window)
-input_data_file.place(x=300,y=200,width=150)
+Load_data = ttk.Button(window,text="Load Data :",command=load_data, bootstyle = (ttk.constants.PRIMARY, ttk.constants.OUTLINE))
+Load_data.place(x=50,y=310,width=150)
+input_data_file = ttk.Entry(window)
+input_data_file.place(x=210,y=310,width=120)
 
 # 'Save Data' button 
-Save_data = Button(window,text="Save Data :", font=font_yh2, command=save_data)
-Save_data.place(x=200,y=275,width=87)
-output_data_file = Entry(window)
-output_data_file.place(x=300,y=280,width=150)
+Save_data = ttk.Button(window,text="Save Data :", command=save_data, bootstyle = (ttk.constants.INFO, ttk.constants.OUTLINE))
+Save_data.place(x=50,y=360,width=150)
+output_data_file = ttk.Entry(window)
+output_data_file.place(x=210,y=360,width=120)
+
+# 'Start' button 
+Start = ttk.Button(window,text="Start",command=regression_plot, bootstyle=ttk.constants.SUCCESS)
+Start.place(x=50,y=420,width=130)
+
+# 'Restart' button 
+Start = ttk.Button(window,text="Clear",command= clear_data, bootstyle=ttk.constants.DANGER)
+Start.place(x=200,y=420,width=130)
+
+# values label
+Max_iter = ttk.Label(window,text="X-Values  |   Y-values",anchor="w", background = '#fff', font = font2)
+Max_iter.place(x=415,y=50,width=330)
 
 # show all X
-list_X = Listbox(window)
-list_X.place(x=50,y=175,width=67)
+list_X = tk.Listbox(window)
+list_X.place(x=400,y=100,width=170)
 
 # show all Y
-list_Y = Listbox(window)
-list_Y.place(x=120,y=175,width=67)
+list_Y = tk.Listbox(window)
+list_Y.place(x=580,y=100,width=170)
 
+lbl = ImageLabel(window)
+lbl.pack()
+lbl.place(x=800,y=10,width=550)
 
-
-
-
-
-
-
-tkinter.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+lbl2 = ImageLabel(window)
+lbl2.pack()
+lbl2.place(x=1350,y=10,width=550)
 
 
 
